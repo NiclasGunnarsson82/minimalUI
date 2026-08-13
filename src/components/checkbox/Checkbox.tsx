45ecfcac03a236a88e20b3ef6c6c31c743996046
NiclasGunnarsson82 <71005102+NiclasGunnarsson82@users.noreply.github.com>
2026-08-13 13:30:24 +0200

    add checkbox components

---
+++ src/components/checkbox/Checkbox.tsx
@@ -0,0 +1,46 @@
+import { CSSProperties, forwardRef } from "react";
+import { useComponentsProvider } from "@/utils/ComponentsContext";
+import scss from "../checkboxes/Checkbox.module.scss"
+import type { CheckboxProps } from "../types";
+
+export const Checkbox = forwardRef<HTMLInputElement, CheckboxProps>(
+    ({  
+        state,
+        required,
+        checkbox,
+        ...props }, ref) => {
+
+    const { tokens } = useComponentsProvider()
+
+    const styles = {
+        "--input-padding": tokens.general.form.inputPadding,
+        "--input-shadow": tokens.general.form.inputShadow,
+        "--checkbox-height": tokens.general.form.checkboxHeight,
+        "--checkbox-width": tokens.general.form.checkboxWidth,
+        "--input-font-size": tokens.general.font.baseSize,
+        "--font-family": tokens.general.font.fontfamily,
+        "--font-weight": tokens.general.font.fontWeightRegular,
+        "--border-radius": tokens.general.form.inputBorderRadius,
+        "--colour-error": tokens.theme.error,
+        "--colour-hover": tokens.colours.c200,
+        "--border-colour": tokens.theme.inputBorder,
+        "--colour-success": tokens.theme.success
+    } as CSSProperties
+    
+    return(
+        <label 
+            className={scss.checkbox}
+            style={styles} 
+            htmlFor={checkbox.option.toLowerCase()}>
+                <input 
+                    ref={ref}
+                    {...props} 
+                    type="checkbox" 
+                    name={checkbox.option.toLowerCase()}
+                    value={checkbox.value}/>
+                    <span className={scss.checkmark}></span>
+                {required && <strong>*</strong>}
+                {checkbox.option}               
+        </label>  
+    )
+})
---
+++ src/components/checkboxlist/CheckboxList.tsx
@@ -0,0 +1,72 @@
+import {  CSSProperties } from "react";
+import { useComponentsProvider } from "@/utils/ComponentsContext";
+import scss from "../checkboxes/Checkbox.module.scss"
+import type { CheckboxGroupProps } from "../types";
+import { alignClass, updateSelected } from "../checkboxes/helpers";
+
+
+export const CheckboxList =
+    ({  
+        state,
+        selected,
+        setSelected,
+        required,
+        align = "vertical",
+        checkboxes,
+        ...props 
+    }: CheckboxGroupProps) => {
+
+    const { tokens } = useComponentsProvider()
+
+    const styles = {
+        "--input-padding": tokens.general.form.inputPadding,
+        "--input-shadow": tokens.general.form.inputShadow,
+        "--checkbox-height": tokens.general.form.checkboxHeight,
+        "--checkbox-width": tokens.general.form.checkboxWidth,
+        "--input-font-size": tokens.general.font.baseSize,
+        "--font-family": tokens.general.font.fontfamily,
+        "--font-weight": tokens.general.font.fontWeightRegular,
+        "--border-radius": tokens.general.form.inputBorderRadius,
+        "--colour-error": tokens.theme.error,
+        "--colour-hover": tokens.colours.c200,
+        "--border-colour": tokens.theme.inputBorder,
+        "--colour-success": tokens.theme.success
+    } as CSSProperties
+
+    const prefix: string = checkboxes.group.toLowerCase()+"-"
+    const alignment: string = alignClass(align, scss) 
+    
+    return(
+        <div className={scss.checkboxList}>
+            <p>
+                {required && <strong>*</strong>}
+                {checkboxes.group}
+            </p>
+            <div className={alignment}>
+                {checkboxes.checkboxes.map((checkbox) => {
+                let identifier: string = prefix+checkbox.option.toLowerCase()
+                    return(
+                        <label 
+                            className={scss.checkbox}
+                            style={styles} 
+                            htmlFor={identifier}>
+                            <input 
+                                 onChange={(event) => 
+                                    updateSelected(
+                                        event, 
+                                        setSelected, 
+                                        selected)} 
+                                type="checkbox"
+                                name={identifier} 
+                                value={checkbox.value}
+                                {...props}/>
+                            <span className={scss.checkmark}></span>
+                            {checkbox.option}
+                        </label> 
+                    )
+                })}
+            </div>
+            
+       </div>              
+    )
+}
