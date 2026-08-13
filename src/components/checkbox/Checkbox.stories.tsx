0b6551fc1dd7088e971d986ae99a0381cbdd9465
NiclasGunnarsson82 <71005102+NiclasGunnarsson82@users.noreply.github.com>
2026-08-13 12:52:16 +0200

    add storybook files for checkbox variants

---
+++ src/components/checkbox/Checkbox.stories.tsx
@@ -0,0 +1,15 @@
+import type { Meta, StoryObj } from "@storybook/react-vite"
+import { Checkbox } from "./Checkbox"
+
+const meta = {
+    title: "Components/Checkbox",
+    component: Checkbox,
+ 
+
+} satisfies Meta<typeof Checkbox>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+
---
+++ src/components/checkboxlist/CheckboxList.stories.tsx
@@ -0,0 +1,16 @@
+import type { Meta, StoryObj } from "@storybook/react-vite"
+import { CheckboxList } from "./CheckboxList"
+
+const meta = {
+    title: "Components/Checkbox list",
+    component: CheckboxList,
+ 
+
+} satisfies Meta<typeof CheckboxList>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+
+
