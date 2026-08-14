e843b200c0062c16cf78799ba3a5ffa64f182bae
NiclasGunnarsson82 <71005102+NiclasGunnarsson82@users.noreply.github.com>
2026-08-14 17:57:33 +0200

    update Checkbox stories and docs

--- src/components/checkbox/Checkbox.stories.tsx
+++ src/components/checkbox/Checkbox.stories.tsx
@@ -4,18 +4,53 @@ import { Checkbox } from "./Checkbox"
 const meta = {
     title: "Components/Checkbox",
     component: Checkbox,
- 
-
+    argTypes: {
+        state: {
+            description: 'Determines the styling of the checkbox and the disabled attribute value.',
+            table: {
+                type: {
+                    summary: '"default" | "error" | "disabled"'
+                }
+            }
+        },
+        required: {
+            description: 'Signifies whether the checkbox is required or not.',
+            table: {
+                type: {
+                    summary: 'boolean'
+                }
+            }
+        },
+        checkbox: {
+            description: 'The checkbox item object.',
+            table: {
+                type: {
+                    summary: '{name: string, label: string, value?: string}'
+                }
+            }
+        },
+    }
 } satisfies Meta<typeof Checkbox>
 
 export default meta
 
 type Story = StoryObj<typeof meta>
 
-export const Terms: Story = {
+export const Default: Story = {
     tags: ["!dev"],
     args: {
         state: "default",
+        checkbox:{
+            name: "terms_conditions", 
+            label: "Accept terms and conditions"
+        }
+    }
+}
+
+export const Disabled: Story = {
+    tags: ["!dev"],
+    args: {
+        state: "disabled",
         checkbox:{
             name: "terms_conditions", 
             label: "Accept terms and conditions"
